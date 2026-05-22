13f25758114e97953bb3c7ad47693440a99897dc
Cristian Felicio <94009542+Crostiam@users.noreply.github.com>
2026-05-22 18:41:36 +0100

    Configure base path in Vite config
    Set base path for deployment to 'RealmWix'.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Replace 'repository-name' with the name of your repo on GitHub
+  base: 'RealmWix', 
+});
