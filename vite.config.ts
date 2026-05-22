a593a9000753880cc166fc7cf71fe471a574f700
Cristian Felicio <94009542+Crostiam@users.noreply.github.com>
2026-05-22 18:43:55 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Replace 'repository-name' with the name of your repo on GitHub
-  base: 'RealmWix', 
+  base: '/RealmWix/', 
 });
