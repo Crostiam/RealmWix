0ec1372dd1a9c9b586fe616440b29f7953ffd5a6
Cristian Felicio <94009542+Crostiam@users.noreply.github.com>
2026-05-25 17:08:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Forces relative paths for all assets
-});
+  // ADD THIS LINE BELOW (Replace 'your-repo-name' with your actual GitHub repository name)
+  base: '/RealmWix/', 
+})
